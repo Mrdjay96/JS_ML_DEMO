48194c6ede6b91f172b60d2d98b88a2dd7626fea
TranTu <mrdjay96@gmail.com>
2020-04-14 10:28:38 +0700

    Add test delay

--- src/app/face-api/bg-subtract/bg-subtract.component.ts
+++ src/app/face-api/bg-subtract/bg-subtract.component.ts
@@ -10,8 +10,6 @@ declare const cv: any;
 
 export class BgSubtractComponent implements OnInit {
 
-    // cv = require('../../../assets/opencv.js');
-
     ngOnInit() {
         const videoElement = document.getElementById('inputVideo') as HTMLVideoElement;
         cv['onRuntimeInitialized'] = () => {
@@ -37,33 +35,37 @@ export class BgSubtractComponent implements OnInit {
     }
 
     start (videoElement) {
+        let timer = null;
         videoElement.width = videoElement.videoWidth;
         videoElement.height = videoElement.videoHeight;
         let cap = new cv.VideoCapture(videoElement);
         let frame = new cv.Mat(videoElement.height, videoElement.width, cv.CV_8UC4);
         console.log(frame.size())
         let fgmask = new cv.Mat(videoElement.height, videoElement.width, cv.CV_8UC1);
-        let bgSubtractor = new cv.BackgroundSubtractorMOG2(500, 16, true);
+        let bgSubtractor = new cv.BackgroundSubtractorMOG2(500, 16, false);
         const canvas = document.getElementById('overlay') as HTMLCanvasElement;
-        const FPS = 30;
+        
         function processVideo() {
             try {
                 if(!videoElement.currentTime || videoElement.paused || videoElement.ended) {
                     return setTimeout(processVideo);
+                    // window.cancelAnimationFrame(timer);
+                    // timer = window.requestAnimationFrame(processVideo.bind(this));
+                    return;
                 }
                 let begin = Date.now();
-                console.log('Start reading...');
                 cap.read(frame);
-                console.log('Readed frame');
                 bgSubtractor.apply(frame, fgmask);
-                console.log('Subtracted background');
-                this.cv.imshow(canvas, fgmask);
-                let delay = 1000/FPS - (Date.now() - begin);
-                setTimeout(processVideo, delay);
+                console.log(cv.countNonZero(fgmask));
+                cv.imshow(canvas, fgmask);
+                setTimeout(processVideo, 100);
+                // window.cancelAnimationFrame(timer);
+                // timer = window.requestAnimationFrame(processVideo.bind(this));
             } catch (err) {
                 console.log(err);
             }
         }
-        setTimeout(processVideo, 0);
+        // setInterval(processVideo, 500);
+        processVideo();
     }
 }
